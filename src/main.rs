1794a915f8ef951aea66c7e9a0b3ae99ae0760ad
drollings <daniel.rollings@gmail.com>
2021-06-15 04:19:20 -0500

    Basic rust argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -44,9 +44,9 @@ bar:
 
 
 fn main() {
-    let matches = App::new("My Super Program")
-                          .version("1.0")
-                          .author("Kevin K. <kbknapp@gmail.com>")
+    let matches = App::new("yamake")
+                          .version("0.4")
+                          .author("Daniel Rollings <daniel.rollings@gmail.com>")
                           .about("Does awesome things")
                           .arg(Arg::with_name("config")
                                .short("c")
@@ -54,18 +54,20 @@ fn main() {
                                .value_name("FILE")
                                .help("Sets a custom config file")
                                .takes_value(true))
-                          .arg(Arg::with_name("INPUT")
-                               .help("Sets the input file to use")
-                               .required(true)
-                               .index(1))
+                          .arg(Arg::with_name("build")
+                               .short("b")
+                               .long("build")
+                               .value_name("FILE")
+                               .help("Sets a custom build file")
+                               .takes_value(true))
                           .arg(Arg::with_name("v")
                                .short("v")
                                .multiple(true)
                                .help("Sets the level of verbosity"))
                           .subcommand(SubCommand::with_name("test")
                                       .about("controls testing features")
-                                      .version("1.3")
-                                      .author("Someone E. <someone_else@other.com>")
+                                      .version("0.1")
+                                      .author("Daniel Rollings <daniel.rollings@gmail.com>")
                                       .arg(Arg::with_name("debug")
                                           .short("d")
                                           .help("print debug information verbosely")))
@@ -85,13 +87,11 @@ fn main() {
 
 
 
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    let config = matches.value_of("config").unwrap_or("default.conf");
+    let config = matches.value_of("config").unwrap_or("yamake_config.yaml");
     println!("Value for config: {}", config);
 
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
+    let build = matches.value_of("build").unwrap_or("yamake.yaml");
+    println!("Value for build: {}", build);
 
     // Vary the output based on how many times the user used the "verbose" flag
     // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
